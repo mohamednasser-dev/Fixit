4f00172aaf12d561be6695be8043255c32d1299b
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-09-14 01:12:50 +0200

    make cloudinary

--- app/Helpers/web_helpers.php
+++ app/Helpers/web_helpers.php
@@ -2,14 +2,14 @@
 
 if (!function_exists('image_cloudinary_url')){
     function image_cloudinary_url(){
-        $url = 'https://res.cloudinary.com/dsrtqoxu2/image/upload/w_100,q_100/v1581928924/';
+        $url = 'https://res.cloudinary.com/ddrwowwpe/image/upload/w_100,q_100/v1581928924/';
         return  $url ;
     }
 }
 
 if (!function_exists('media_image_cloudinary_url')){
     function media_image_cloudinary_url(){
-        $url = 'https://res.cloudinary.com/dsrtqoxu2/image/upload/v1581928924/';
+        $url = 'https://res.cloudinary.com/ddrwowwpe/image/upload/v1581928924/';
         return  $url ;
     }
 }
--- app/Http/Middleware/CheckGuest.php
+++ app/Http/Middleware/CheckGuest.php
@@ -20,7 +20,7 @@ class CheckGuest
             return $next($request);
         }else{
             $gusetkey = $request->header('Authorization');
-            if($gusetkey == '$2y$12$ZtgKLOyfvyXH33JE67Ei0jqupt771t62d21M4wOJumBmsZ1bexxpCPiuhfdRKOH'){
+            if($gusetkey == '$2y$12$ZtgKLOyfvyXH33JE67Ei0jqupt771t62d21M4wOJumBmsZ1bexxpCPiuhfdRKOHF'){
                 return $next($request);
             }
             $response = APIHelpers::createApiResponse(true , 401 ,  'Guest token wrong', 'توكن زائر خاطيء', null,$request->lang );
