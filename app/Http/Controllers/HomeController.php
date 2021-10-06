29615a45ccbdfa8a07871b634055cca29f9531a7
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-10-06 17:23:38 +0200

    make orders

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,15 +24,15 @@ class HomeController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['my_orders','add_order','offers','city_filter','balance_packages', 'gethome', 'main_page', 'check_ad', 'main_ad']]);
+        $this->middleware('auth:api', ['except' => ['my_orders', 'add_order', 'offers', 'city_filter', 'balance_packages', 'gethome', 'main_page', 'check_ad', 'main_ad']]);
         //        --------------------------------------------- begin scheduled functions --------------------------------------------------------
-            $expired = Product::where('status', 1)->whereDate('expiry_date', '<', Carbon::now())->get();
-            foreach ($expired as $row) {
-                $product = Product::find($row->id);
-                $product->status = 2;
-                $product->re_post = '0';
-                $product->save();
-            }
+        $expired = Product::where('status', 1)->whereDate('expiry_date', '<', Carbon::now())->get();
+        foreach ($expired as $row) {
+            $product = Product::find($row->id);
+            $product->status = 2;
+            $product->re_post = '0';
+            $product->save();
+        }
         //        --------------------------------------------- end scheduled functions --------------------------------------------------------
     }
 
@@ -62,7 +62,8 @@ class HomeController extends Controller
         return response()->json($response, 200);
     }
 
-    public function main_page(Request $request){
+    public function main_page(Request $request)
+    {
         $lang = $request->lang;
         $data['slider'] = Ad::select('id', 'image', 'type', 'content')->where('place', 1)->get();
         $products = Category::where('deleted', 0)->select('id', 'title_' . $lang . ' as title', 'image')
@@ -70,26 +71,27 @@ class HomeController extends Controller
         $new_ad = [];
         $ad_data = [];
         for ($i = 0; $i < count($products); $i++) {
-            if ((($i+1) % 6) == 0) {
+            if ((($i + 1) % 6) == 0) {
                 $ad = Ad::select('id', 'image', 'type', 'content')->where('place', 3)->inRandomOrder()->first();
-                if($ad){
-                    $ad_data['id']= 0;
+                if ($ad) {
+                    $ad_data['id'] = 0;
                     $ad_data['title'] = $ad->content;
                     $ad_data['image'] = $ad->image;
                     $ad_data['type'] = $ad->type;
-                    array_push($new_ad , $products[$i]);
-                    array_push($new_ad , $ad_data);
+                    array_push($new_ad, $products[$i]);
+                    array_push($new_ad, $ad_data);
                 }
-            }else{
-                array_push($new_ad , $products[$i]);
+            } else {
+                array_push($new_ad, $products[$i]);
             }
         }
-        $data['services'] = $new_ad ;
+        $data['services'] = $new_ad;
         $response = APIHelpers::createApiResponse(false, 200, '', '', $data, $request->lang);
         return response()->json($response, 200);
     }
 
-    public function offers(Request $request){
+    public function offers(Request $request)
+    {
         $data = Ad::select('id', 'image', 'type', 'content')->where('place', 3)->get();
         $response = APIHelpers::createApiResponse(false, 200, '', '', $data, $request->lang);
         return response()->json($response, 200);
@@ -101,7 +103,7 @@ class HomeController extends Controller
     {
         $data = Main_ad::select('image')->where('deleted', '0')->inRandomOrder()->take(1)->get();
         if (count($data) == 0) {
-            $response = APIHelpers::createApiResponse(true, 406, 'no ads available','لا يوجد اعلانات', null, $request->lang);
+            $response = APIHelpers::createApiResponse(true, 406, 'no ads available', 'لا يوجد اعلانات', null, $request->lang);
             return response()->json($response, 406);
         }
         foreach ($data as $image) {
@@ -110,25 +112,26 @@ class HomeController extends Controller
         $response = APIHelpers::createApiResponse(false, 200, '', '', $image, $request->lang);
         return response()->json($response, 200);
     }
-    public function city_filter(Request $request,$area_id)
+
+    public function city_filter(Request $request, $area_id)
     {
         $user = auth()->user();
         $lang = $request->lang;
         Session::put('api_lang', $lang);
         $lang = $request->lang;
         $products = Product::where('status', 1)
-                            ->with('Publisher')
-                            ->where('publish', 'Y')
-                            ->where('deleted', 0)
-                            ->where('area_id', $area_id)
-                            ->select('id', 'title', 'main_image as image', 'created_at', 'user_id','city_id','area_id')
-                            ->orderBy('created_at', 'desc')
-                            ->simplePaginate(12);
+            ->with('Publisher')
+            ->where('publish', 'Y')
+            ->where('deleted', 0)
+            ->where('area_id', $area_id)
+            ->select('id', 'title', 'main_image as image', 'created_at', 'user_id', 'city_id', 'area_id')
+            ->orderBy('created_at', 'desc')
+            ->simplePaginate(12);
         for ($i = 0; $i < count($products); $i++) {
-            if($lang == 'ar'){
-                $products[$i]['address'] = $products[$i]['City']->title_ar .' , '.$products[$i]['Area']->title_ar;
-            }else{
-                $products[$i]['address'] = $products[$i]['City']->title_en .' , '.$products[$i]['Area']->title_en;
+            if ($lang == 'ar') {
+                $products[$i]['address'] = $products[$i]['City']->title_ar . ' , ' . $products[$i]['Area']->title_ar;
+            } else {
+                $products[$i]['address'] = $products[$i]['City']->title_en . ' , ' . $products[$i]['Area']->title_en;
             }
 
             if ($user) {
@@ -166,10 +169,11 @@ class HomeController extends Controller
         $response = APIHelpers::createApiResponse(false, 200, '', '', $data, $request->lang);
         return response()->json($response, 200);
     }
+
     public function add_order(Request $request)
     {
         $data = $request->all();
-        $user = auth()->user() ;
+        $user = auth()->user();
         if (!$user) {
             $response = APIHelpers::createApiResponse(true, 406, 'you should login first', 'يجب تسجيل الدخول اولا', (object)[], $request->lang);
             return response()->json($response, 406);
@@ -190,16 +194,18 @@ class HomeController extends Controller
         $response = APIHelpers::createApiResponse(false, 200, '', '', $order, $request->lang);
         return response()->json($response, 200);
     }
+
     public function my_orders(Request $request)
     {
-        $lang = $request->lang ;
+        $lang = $request->lang;
         Session::put('api_lang', $lang);
-        $user = auth()->user() ;
+        $user = auth()->user();
         if (!$user) {
             $response = APIHelpers::createApiResponse(true, 406, 'you should login first', 'يجب تسجيل الدخول اولا', (object)[], $request->lang);
             return response()->json($response, 406);
         }
-        $order = Order::select('id','product_id')->with('Product')->where('user_id',$user->id)->get()->makeHidden(['product_categories']);
+        $order = Order::select('id', 'product_id','status','visit_date','visit_time')
+            ->with('Product')->where('user_id', $user->id)->get()->makeHidden(['Product_categories']);
         $response = APIHelpers::createApiResponse(false, 200, '', '', $order, $request->lang);
         return response()->json($response, 200);
     }
--- app/Order.php
+++ app/Order.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
@@ -10,5 +11,16 @@ class Order extends Model
 
     public function Product() {
         return $this->belongsTo('App\Product', 'product_id')->select('id','main_image as image','status','title_'.session('api_lang').' as title');
+    }
+
+    public function  getVisitTimeAttribute($data){
+        return Carbon::createFromFormat('H:i:s', $data)->format('g:i a');
+    }
+    public function  getVisitDateAttribute($data){
+        if(session('local_api') == 'ar') {
+            return Carbon::createFromFormat('Y-m-d', $data)->translatedformat('Y l d F');
+        }else{
+            return Carbon::createFromFormat('Y-m-d', $data)->format('Y F d l');
+        }
     }
 }
